993d06a2f87caed2fe58370572176494e3a08552
Brutus <dev.brutus@gmail.com>
2015-03-22 17:25:33 +0300

    Научиться рисовать окно

--- src/main/java/com/github/dev_brutus/bagel_ui/impl/DisplayWindow.java
+++ src/main/java/com/github/dev_brutus/bagel_ui/impl/DisplayWindow.java
@@ -1,16 +1,30 @@
 package com.github.dev_brutus.bagel_ui.impl;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  *
  */
 public class DisplayWindow extends JFrame {
-    DisplayImpl display;
+    public static final int SYMBOL_HEIHGT = 16;
+    public static final int SYMBOL_WIDTH = 8;
 
-    public DisplayWindow(DisplayImpl display) {
+    private final BufferedImage image;
+    private final DisplayImpl display;
+
+
+    public DisplayWindow(DisplayImpl display) throws IOException {
         this.display = display;
+
+        InputStream resourceAsStream = DisplayWindow.class.getResourceAsStream("/com/github/dev_brutus/bagel_ui/font.gif");
+        image = ImageIO.read(resourceAsStream);
+        resourceAsStream.close();
+
         setName("Bagel UI");
         setBackground(Color.BLACK);
         setVisible(true);
